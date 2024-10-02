ca948f911af155d89799e32b6c044711da634623
John Chan <cch235uclive.ac.nz>
2024-10-02 16:15:25 +1300

    updated story entity

--- app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/models/Story.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/models/Story.kt
@@ -1,4 +1,11 @@
 package nz.ac.canterbury.seng303.scrumboardmobile.models
 
-data class Story {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Story (
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "Title") val title: String
+)
