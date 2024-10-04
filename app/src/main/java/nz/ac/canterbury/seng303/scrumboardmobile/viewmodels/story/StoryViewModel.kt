53335356c8967d2386c6d3c2290a29659a96cee1
Yunu Cho <ych226@uclive.ac.nz>
2024-10-04 14:22:48 +1300

    Added story card, and an add task button

--- app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/viewmodels/story/StoryViewModel.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/viewmodels/story/StoryViewModel.kt
@@ -6,34 +6,47 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import nz.ac.canterbury.seng303.scrumboardmobile.dao.StoryDao
 import nz.ac.canterbury.seng303.scrumboardmobile.models.Story
+import nz.ac.canterbury.seng303.scrumboardmobile.models.StoryWithTasks
 
-class StoryViewModel (
-    private val storyDao: StoryDao
-    ): ViewModel() {
-        private val _stories = MutableStateFlow<List<Story>>(emptyList())
-        val stories: StateFlow<List<Story>> get() = _stories
-        fun getStories() = viewModelScope.launch {
-            storyDao.getAllStories().catch { Log.e("STORY_VIEW_MODEL", it.toString()) }
-                .collect { _stories.emit(it) }
-        }
+class StoryViewModel (private val storyDao: StoryDao): ViewModel() {
+    private val _stories = MutableStateFlow<List<Story>>(emptyList())
+    val stories: StateFlow<List<Story>> get() = _stories
 
-        fun createStory(title: String,
-                        description: String,
-                        timeCreated: Long) = viewModelScope.launch {
-            val story = Story(
-                title = title,
-                description = description,
-                timeCreated = timeCreated
-            )
-            try {
-                val storyId = storyDao.insertStory(story)
-                Log.d("STORY_VIEW_MODEL", "Story has been created with id: $storyId")
-            } catch (e: Exception) {
-                Log.e("STORY_VIEW_MODEL", "Could not insert Story", e)
-            }
+    private val _selectedStoryWithTasks = MutableStateFlow<StoryWithTasks?>(null)
+    val selectedStoryWithTasks: StateFlow<StoryWithTasks?> = _selectedStoryWithTasks
+
+    fun getStories() = viewModelScope.launch {
+        storyDao.getAllStories().catch { Log.e("STORY_VIEW_MODEL", it.toString()) }
+            .collect { _stories.emit(it) }
+    }
+
+    fun createStory(title: String,
+                    description: String,
+                    timeCreated: Long) = viewModelScope.launch {
+        val story = Story(
+            title = title,
+            description = description,
+            timeCreated = timeCreated
+        )
+        try {
+            val storyId = storyDao.insertStory(story)
+            Log.d("STORY_VIEW_MODEL", "Story has been created with id: $storyId")
+        } catch (e: Exception) {
+            Log.e("STORY_VIEW_MODEL", "Could not insert Story", e)
         }
+    }
+
+    fun getStoryWithTasks(storyId: Int?) = viewModelScope.launch {
+        if (storyId != null) {
+            _selectedStoryWithTasks.value = storyDao.getStoryWithTasks(storyId).first()
+        } else {
+            _selectedStoryWithTasks.value = null
+        }
+    }
+
 }
 
