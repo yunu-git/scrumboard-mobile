7b7a71df8bf7b79826eeb23a74353e11e6bf67ff
Joshua Kim <bki42@uclive.ac.nz>
2024-10-11 00:42:40 +1300

    final commit (hopefully)

--- app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/screens/task/CreateTaskScreen.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/scrumboardmobile/screens/task/CreateTaskScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -27,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -204,7 +206,9 @@ fun CreateTaskScreen (
                 label = { Text(ContextCompat.getString(context, R.string.task_estimate)) },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 8.dp)
+                    .padding(vertical = 8.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
             )
 
             Button(
